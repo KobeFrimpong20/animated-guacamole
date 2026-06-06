f589fa4048718d98b96bfff3cff12a0dbeb4625a
Kwame Opoku Frimpong <kwame@Kwames-MacBook-Pro.local>
2026-06-06 13:29:54 -0400

    Revert "update auth"
    This reverts commit 3374b20c090600157bed2e3f3af663129a53f0ad.

--- loop-learn/lib/supabase/proxy.ts
+++ loop-learn/lib/supabase/proxy.ts
@@ -38,20 +38,15 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  const isAuthRoute =
-    request.nextUrl.pathname.startsWith('/login') ||
-    request.nextUrl.pathname.startsWith('/signup')
-
-  if (!user && !isAuthRoute) {
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth')
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
-    return NextResponse.redirect(url)
-  }
-
-  if (user && isAuthRoute) {
-    const url = request.nextUrl.clone()
-    url.pathname = '/'
-    return NextResponse.redirect(url)
+    // return NextResponse.redirect(url)
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
